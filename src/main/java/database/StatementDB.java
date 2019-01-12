32d03cc9bb7708f4dc777628462cc9c012c48b1b
comendantOfautomation <zdp94@ukr.net>
2019-01-12 20:11:52 +0200

    Refactored methods for connecting to DB. Excluded possibility of NullPointerException while getting result of query from DB

--- src/main/java/database/StatementDB.java
+++ src/main/java/database/StatementDB.java
@@ -2,7 +2,6 @@ package database;
 
 import org.apache.log4j.Logger;
 
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -13,7 +12,7 @@ public class StatementDB {
     private ResultSet resultSet = null;
     private Logger log = Logger.getLogger(StatementDB.class);
 
-    public ResultSet getResultSet(DBConnection connection, String query) {
+    private ResultSet getResultSet(DBConnection connection, String query) {
 
         try {
             log.debug("Try to get statement ");
@@ -21,17 +20,18 @@ public class StatementDB {
         } catch (SQLException e) {
             log.error("ERROR! Can't get statement");
             log.error("The stacktrace: " + e.getMessage());
-
         }
         if (statement != null) {
             log.debug("The statement was set successfully!");
         }
-        try {
-            log.debug("Try to resolve resultSet");
-            resultSet = statement.executeQuery(query);
-            resultSet.next();
-        } catch (SQLException e) {
-            log.error("ERROR! The result set was not set. The stackTrace is :" + e.getMessage());
+        if (statement != null) {
+            try {
+                log.debug("Try to resolve resultSet");
+                resultSet = statement.executeQuery(query);
+                resultSet.next();
+            } catch (SQLException e) {
+                log.error("ERROR! The result set was not set. The stackTrace is :" + e.getMessage());
+            }
         }
         if (resultSet != null) {
             log.debug("ResultSet was set successfully");
@@ -39,7 +39,7 @@ public class StatementDB {
         return resultSet;
     }
 
-    public void closeStatement() {
+    private void closeStatement() {
         try {
             log.debug("Try to close statement");
             statement.close();
@@ -77,45 +77,44 @@ public class StatementDB {
         if (statement != null) {
             log.debug("The statement was set successfully!");
         }
-        try {
-            log.debug("Try to resolve resultSet");
-            statement.executeUpdate(query);
-            // resultSet.next();
-        } catch (SQLException e) {
-            log.error("ERROR! The result set was not set. The stackTrace is :" + e.getMessage());
+        if (statement != null) {
+            try {
+                log.debug("Try to resolve resultSet");
+                statement.executeUpdate(query);
+            } catch (SQLException e) {
+                log.error("ERROR! The result set was not set. The stackTrace is :" + e.getMessage());
+            }
         }
-
 
     }
 
     public String getStringFromResultSet(String query, String column) {
         DBConnection connection = new DBConnection();
-        ResultSet resultSet = getResultSet(connection, query);
-        try {
-            log.info("Try to get string from resultSet " + resultSet + " the columnName is: " + column);
-            String string = resultSet.getString(column);
-            closeResultSet(resultSet);
-            closeStatement();
-            connection.closeConnection();
-            return string;
-        } catch (SQLException e) {
-            log.error("ERROR! Can't get string from result set: " + e.getSQLState() + " " + e.getMessage());
-            e.printStackTrace();
-
-            return null;
-        } catch (NullPointerException e) {
-            log.error("ERROR! Got NullPointerException! " + e.getMessage());
-            return "";
-        }
+        ResultSet resultSetForQuery = getResultSet(connection, query);
+        if (resultSetForQuery != null) {
+            try {
+                log.info("Try to get string from DB. Column name: " + "'" + column + "'");
+                String string = resultSetForQuery.getString(column);
+                closeResultSet(resultSetForQuery);
+                closeStatement();
+                connection.closeConnection();
+                return string;
+            } catch (SQLException e) {
+                log.error("ERROR! Can't get string from result set: " + e.getSQLState() + " " + e.getMessage());
+                return null;
+            } catch (NullPointerException e) {
+                log.error("ERROR! Got NullPointerException! " + e.getMessage());
+                return "";
+            }
+        } else return null;
     }
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
 
         StatementDB statementDB = new StatementDB();
         String column1 = "somevalue";
         String sql = "select * from postingdata where FILEID=4 order by somevalue desc";
-        String stringFromResultSet = statementDB.getStringFromResultSet( sql, column1);
-
+        String stringFromResultSet = statementDB.getStringFromResultSet(sql, column1);
         System.out.println(stringFromResultSet);
     }
 }
